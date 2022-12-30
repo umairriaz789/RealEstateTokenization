3dd841ae868b71da24db53cb9386c31f939f6d7f
umair riaz <umairriaz789@gmail.com>
2022-12-30 01:19:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors());
 
 app.use(express.static(path.join(__dirname , "./frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname , "./frontend/dist/index.html"));
 });
 
